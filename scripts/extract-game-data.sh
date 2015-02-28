4ea76592f40e63e355b9d28b1d5a91f697502a08
James Young <jmeyoung@gmail.com>
2015-02-28 11:52:21 -0500

    fix(shellcheck): update to pass shellcheck, refactor a bit

--- scripts/extract-game-data.sh
+++ scripts/extract-game-data.sh
@@ -3,19 +3,19 @@
 escape="\033";
 green="${escape}[32m";
 yellow="${escape}[33m";
-red="${escape}[31m";
 reset="${escape}[0m";
 INFO="\n[${green}INFO${reset}]"
-ERROR="[${red}ERROR${reset}]"
 WARNING="\n[${yellow}WARNING${reset}]"
 
-PREVIOUS_VERSION=$(cat json/info.json | jq -r '.BlizzardFileVersion')
+HEARTHSTONE_PATH=/Applications/Hearthstone
+
+PREVIOUS_VERSION=$(jq -r '.BlizzardFileVersion' json/info.json)
 
 
 
 echo -e "${INFO} Checking for Hearthstone update"
-plutil -convert json /Applications/Hearthstone/Hearthstone.app/Contents/info.plist -o json/info.json
-CURRENT_VERSION=$(cat json/info.json | jq -r '.BlizzardFileVersion')
+plutil -convert json ${HEARTHSTONE_PATH}/Hearthstone.app/Contents/info.plist -o json/info.json
+CURRENT_VERSION=$(jq -r '.BlizzardFileVersion' json/info.json)
 
 if [ "${PREVIOUS_VERSION}" == "${CURRENT_VERSION}" ]; then
     echo -e "${INFO} Version found: ${CURRENT_VERSION} - No change"
@@ -28,22 +28,28 @@ fi
 
 if [ "${CHANGE_FOUND}" == true ];  then
     echo -e "${INFO} Extracting xml from cardxml0.unity3d"
-    ../disunity/disunity.sh extract /Applications/Hearthstone/Data/OSX/cardxml0.unity3d
+    ../disunity/disunity.sh extract ${HEARTHSTONE_PATH}/Data/OSX/cardxml0.unity3d
 
 
-
+    # This is redundant, but I am doing it anyway.  A `git diff` will catch if
+    # anything unexpected has happened after running this script.
     echo -e "${INFO} Backing up current enUS.xml to enUS-${PREVIOUS_VERSION}.xml"
-    mv -v xml/enUS.xml xml/enUS-${PREVIOUS_VERSION}.xml
-    
+    mv -v xml/enUS.xml "xml/enUS-${PREVIOUS_VERSION}.xml"
 
 
-    echo -e "${INFO} Moving xml into project"
-    mv -v /Applications/Hearthstone/Data/OSX/cardxml0/CAB-cardxml0/TextAsset/enUS.txt xml/enUS.xml
+
+    echo -e "${INFO} Moving new xml into project"
+    mv -v ${HEARTHSTONE_PATH}/Data/OSX/cardxml0/CAB-cardxml0/TextAsset/enUS.txt xml/enUS.xml
+
+
+
+    echo -e "${INFO} Backing up new enUS.xml to enUS-${CURRENT_VERSION}.xml"
+    cp -v xml/enUS.xml "xml/enUS-${CURRENT_VERSION}.xml"
 
 
 
     echo -e "${INFO} Cleaning up"
-    rm -r /Applications/Hearthstone/Data/OSX/cardxml0
+    rm -r ${HEARTHSTONE_PATH}/Data/OSX/cardxml0
 fi
 
 
